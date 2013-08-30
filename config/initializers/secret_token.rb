3b9ced9083a145bdcaed13046d667121d23aa4f6
Anita Jaszewska <anita.jaszewska@wimdu.com>
2013-08-30 19:05:25 +0200

    initial2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PopcornNCoffee::Application.config.secret_token = 'e056cf2fea30cb4275d542269ee053236d7b5310a238e62e7fbfa963de40964edef89cb1b4ffe404459c25e8017ef41861f5548cb88c4bdb988bc665349d5896'
